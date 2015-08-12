cc92a0a20f26ad47830ec24dc2d55651dd2ec9cf
MrGodin <benwold@telus.net>
2015-08-11 23:33:23 -0700

    umm

---
+++ Burgh/EnemyMoveLeft.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "ObjectState.h"
+#include "StateCores.h"
+class EnemyMoveLeft : public EnemyState
+{
+public:
+	EnemyMoveLeft(EnemyCore *core);
+	~EnemyMoveLeft();
+
+	void Update(float dt)override;
+	void OnCtrlDirChange(TDirection d)override;
+	void OnCtrlJumpPress()override;
+	void OnUnsupported()override;
+	void OnCollision(const RectF rect, const RectF FRect)override;
+
+};
+
